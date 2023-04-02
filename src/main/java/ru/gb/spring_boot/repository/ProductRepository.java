0e053a12fa4f9e53641daa54afa90dadc129616a
Andrey Dubovitskiy <aed_baykal@mail.ru>
2023-04-02 21:30:13 +0300

    fix убрал мусор

--- src/main/java/ru/gb/spring_boot/repository/ProductRepository.java
+++ src/main/java/ru/gb/spring_boot/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package ru.gb.spring_boot.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import ru.gb.spring_boot.dto.Product;
 
@@ -10,14 +12,17 @@ import java.util.List;
 @Component
 public class ProductRepository {
 
+    @Autowired
+    private Environment env;
+
     private List<Product> productList;
 
     @PostConstruct
     public void init() {
         productList = new ArrayList<>();
-        productList.add(new Product(1, "Прод1", 10));
-        productList.add(new Product(2, "Прод2", 20));
-        productList.add(new Product(3, "Прод3", 30));
+        productList.add(new Product("Прод1", 10));
+        productList.add(new Product("Прод2", 20));
+        productList.add(new Product("Прод3", 30));
     }
 
     public List<Product> getAll() {
@@ -29,9 +34,46 @@ public class ProductRepository {
     }
 
     public Product findById(int id) {
-        for (Product product : productList) {
-            if (product.getId() == id) return product;
-        }
+            for (Product product : productList) {
+                if (product.getId() == id) return product;
+            }
         return null;
     }
+
+//    @Autowired
+//    @Bean(name = "sessionFactory")
+//    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+//        Properties properties = new Properties();
+//        properties.put("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+//        properties.put("hibernate.show_sql", env.getProperty("spring.jpa.show-sql"));
+//        properties.put("current_session_context_class",
+//                env.getProperty("spring.jpa.properties.hibernate.current_session_context_class"));
+//
+//        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+//        factoryBean.setPackagesToScan("");
+//        factoryBean.setDataSource(dataSource);
+//        factoryBean.setHibernateProperties(properties);
+//        factoryBean.afterPropertiesSet();
+//
+//        SessionFactory sf = factoryBean.getObject();
+//        return sf;
+//    }
+//
+//    @Autowired
+//    @Bean(name = "transactionManager")
+//    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+//        return transactionManager;
+//    }
+//
+//    @Bean(name = "dataSource")
+//    public DataSource getDataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//
+//        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+//        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+//        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+//        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+//        return dataSource;
+//    }
 }
